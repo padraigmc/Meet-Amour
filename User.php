dcbd01c654812f064622793551bf2b821a896d7f
Pádraig McCarthy <44905245+padraigmc@users.noreply.github.com>
2020-03-13 12:34:29 +0000

    sync with pc from laptop - padraig

--- User.php
+++ User.php
@@ -1,7 +1,5 @@
 <?php
 
-class User 
-{
     public $username;
     public $email;
     public $passwordHash;
@@ -11,13 +9,42 @@ class User
     public $isBanned;
     public $isDeactivated;
     public $isVerified;
+    public $error = array();
 
 
     // =========================================================================================
     //  Getters
     // =========================================================================================
-    function get_attributes() {
+    function get_all_attributes() {
         // prepared statement to read all cols in User table here, set all public vars above
+    } 
+    
+    function get_user_attribute($atrribute, $userID) {
+
+        $error = array();
+
+        $conn = dbConnect();
+        $sql = "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = `User`";
+
+        if (!mysqli_query($conn, $sql)) {
+            die("Error: SQL query failed! (get_attribute())");
+        }
+
+        $cols = mysqli_fetch_assoc($result);
+
+        if (in_array($atrribute, $cols) {
+
+            // prepare and bind statement
+            $selectAttribuet = $conn->prepare("SELECT ? FROM User WHERE userID = ?;");
+            $updateEmail->bind_param("ss", $atrribute, $userID);
+
+            // execute prepared statement
+            $updateEmail->execute();
+
+            $result = $stmt->get_result()->fetch_assoc();
+
+            return $result[$atrribute];
+        }
     }
 
     function get_username() {
@@ -60,20 +87,46 @@ class User
     // =========================================================================================
     //  Setters
     // =========================================================================================
-    function set_attributes($username, $email, $passwordHash, $dateCreated, $lastLogin, $isAdmin, $isBanned, $isDeactivated, $isVerified) {
+    function set_all_attributes($username, $email, $passwordHash, $dateCreated, $lastLogin, $isAdmin, $isBanned, $isDeactivated, $isVerified) {
         // prepared statement to read all cols in User table here, set all public vars above
     }
 
-    function set_() {
-        
+    function set_attribute() {
+
     }
 
     function set_username($username_new) {
-        // prepared statement here
+        // Set up db connection
+        $conn = dbConnect();
+
+        // prepare and bind statement
+        $updateEmail = $dbConnection->prepare("UPDATE User SET username = ? WHERE username = ?;");
+        $updateEmail->bind_param("sss", $username_new, get_username());
+
+        // execute prepared statement
+        $updateEmail->execute();
+        
+        // test if update statement was unsuccessful
+        if (get_username()!=$username_new) {
+            $this ->$error[] = "Error: set_username() unsuccessful";
+        }
     }
 
     function set_email($email_new) {
-        // prepared statement here
+        // Set up db connection
+        $conn = dbConnect();
+
+        // prepare and bind statement
+        $updateEmail = $dbConnection->prepare("UPDATE User SET email = ? WHERE email = ?;");
+        $updateEmail->bind_param("ss", $username_new, $this ->$username);
+
+        // execute prepared statement
+        $updateEmail->execute();
+        
+        // test if update statement was unsuccessful
+        if (get_username()!=$username_new) {
+            $this ->$error[] = "Error: set_username() unsuccessful";
+        }
     }
 
     function set_passwordHash($passwordHash_new) {
@@ -104,15 +157,36 @@ class User
         // prepared statement here
     }
 
-    
+
     // =========================================================================================
     //  Methods
     // =========================================================================================
 
-    public function registerUser($username_new, $email_new, $passwordHash_new) {
-        set
+
+
+    function dbConnect() {
+        // set db connection variables
+        $dbServerName = "localhost";
+        $dbUsername = "root";
+        $dbPassword = "";
+        $dbName = "meetamour";
+
+        // Create connection
+        $conn = new mysqli($dbServerName, $dbUsername, $dbPassword, $dbName);
+
+        // Check connection
+        if ($conn->connect_error) {
+            return NULL;
+        } else {
+            return $conn;
+        }
     }
-}
+
+    public function registerUser($username_new, $email_new, $passwordHash_new) {
+        
+        
+        set_username($user);
+    }
 
 
 ?>
